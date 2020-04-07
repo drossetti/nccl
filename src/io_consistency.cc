46e4e8c6d0eb9d96fc87b327004c3c6efd6b3151
Davide Rossetti <drossetti@nvidia.com>
2020-04-07 16:36:09 -0700

    resync with latest etbl; fix narrowing warn turned into an error

--- src/io_consistency.cc
+++ src/io_consistency.cc
@@ -11,21 +11,7 @@
 extern "C" {
 #endif // __cplusplus
 
-static const CUuuid CU_ETID_IoConsistency = { { 0x87, 0xe8, 0xff, 0x1d, 0xd6, 0x16, 0x57, 0x4b, 0xab, 0x36, 0x02, 0x97, 0x0a, 0x8c, 0x53, 0xb2 } };
-
-/**
- * ETBL Versioning
- *
- * In general, version numbers will be incremented as follows:
- * - When a backwards-compatible change is made to the structure layout, the
- *   minor version for that structure will be incremented. Applications
- *   built against an older minor version will continue to work with the newer
- *   minor version of the APIs without recompilation.
- * - When a breaking change is made to the APIs, the major version
- *   will be incremented. Applications built against an older major
- *   version require at least recompilation and potentially additional updates
- *   to use the new API.
- */
+static const CUuuid CU_ETID_IoConsistency = { { (char)0x87, (char)0xe8, (char)0xff, (char)0x1d, (char)0xd6, (char)0x16, (char)0x57, (char)0x4b, (char)0xab, (char)0x36, (char)0x02, (char)0x97, (char)0x0a, (char)0x8c, (char)0x53, (char)0xb2 } };
 
 #define CU_IOCONSISTENCY_VERSION 0x00010000
 #define CU_IOCONSISTENCY_MAJOR_VERSION_MASK   0xffff0000
@@ -40,12 +26,8 @@ static const CUuuid CU_ETID_IoConsistency = { { 0x87, 0xe8, 0xff, 0x1d, 0xd6, 0x
 #define CU_IOCONSISTENCY_MAJOR_VERSION_MATCHES(v) \
     (CU_IOCONSISTENCY_MAJOR_VERSION(v) == CU_IOCONSISTENCY_MAJOR_VERSION(CU_IOCONSISTENCY_VERSION))
 
-#define CU_IOCONSISTENCY_VERSION_COMPATIBLE(v)    \
-    (CU_IOCONSISTENCY_MAJOR_VERSION_MATCHES(v) && \
-    (CU_IOCONSISTENCY_MINOR_VERSION(v) >= (CU_IOCONSISTENCY_MINOR_VERSION(CU_IOCONSISTENCY_VERSION))))
-
 typedef enum CUioConsistencyAttribute_enum {
-    CU_IOCONSISTENCY_ATTRIBUTE_API_VERSION = 0,     /**< API version implemented in this ETBL. Use the CU_IOCONSISTENCY_VERSION_COMPATIBLE() predicate to check for compatibility. */
+    CU_IOCONSISTENCY_ATTRIBUTE_API_VERSION = 0, /**< API version implemented in this ETBL. */
     CU_IOCONSISTENCY_ATTRIBUTE_SUPPORT_HOSTSIDE_FENCE = 1, /**< returns true if the device supports host-side IO fence. */
     CU_IOCONSISTENCY_ATTRIBUTE_MAX
 } CUioConsistencyAttribute;
@@ -84,33 +66,13 @@ typedef struct CUetblIoConsistency_st {
      * \sa ::ioConsistencyFenceCtx, ::ioConsistencyFenceCurrentCtx
      */
     CUresult (CUDAAPI *ioConsistencyGetAttribute)(int *pi, CUioConsistencyAttribute attrib, CUdevice dev);
-    
-    /**
-     * \brief Issue a fence for ingress traffic originating from third
-     * party devices
-     *
-     * TBD
-     *
-     * \param dev        - The device on which to issue the operation.
-     *
-     * \return
-     * ::CUDA_SUCCESS,
-     * ::CUDA_ERROR_NOT_SUPPORTED,
-     * ::CUDA_ERROR_DEINITIALIZED,
-     * ::CUDA_ERROR_NOT_INITIALIZED,
-     * ::CUDA_ERROR_INVALID_DEVICE,
-     * \notefnerr
-     *
-     * \sa ::ioConsistencyFenceCurrentCtx
-     */
-    CUresult (CUDAAPI *ioConsistencyFenceCtx)(CUcontext ctx);
 
     /**
      * \brief Issue a fence for ingress traffic originating from third
      * party devices
      *
-     * This is equivalent to cuCtxGetCurrent() followed by
-     * ioConsistencyFenceCtx(), and is provided as an optimization.
+     * This is equivalent to cuCtxGetDevice() followed by
+     * ioConsistencyFenceDevice(), and is provided as an optimization.
      *
      * \return
      * ::CUDA_SUCCESS,
@@ -120,9 +82,11 @@ typedef struct CUetblIoConsistency_st {
      * ::CUDA_ERROR_INVALID_CONTEXT
      * \notefnerr
      *
-     * \sa ::ioConsistencyFenceCtx
+     * \sa ::ioConsistencyFenceDevice
      */
     CUresult (CUDAAPI *ioConsistencyFenceCurrentCtx)();
+
+    void *reserved[64];
 
 } CUetblIoConsistency;
 
@@ -143,7 +107,7 @@ static CUresult ioConsistencyGetAttribute(int *pi, CUioConsistencyAttribute attr
     return iocons->ioConsistencyGetAttribute(pi, attrib, dev);
 }
 
-CUresult ioConsistencyDeviceSupportsCpuFlush(int *pflag, int devId)
+CUresult ioConsistencyDeviceSupportsHostSideFence(int *pflag, int devId)
 {
     CUresult rc;
     int attr = 0;
@@ -164,15 +128,9 @@ out:
 
 CUresult ioConsistencyFenceCurrentCtx()
 {
-    ASSERT(iocons);
-    return iocons->ioConsistencyFenceCurrentCtx();
-}
-
-CUresult ioConsistencyFenceCtx(CUcontext ctx)
-{
     if (VALID_ETBL(iocons))
         return CUDA_ERROR_UNKNOWN;
-    return iocons->ioConsistencyFenceCtx(ctx);
+    return iocons->ioConsistencyFenceCurrentCtx();
 }
 
 CUresult ioConsistencyInit()
@@ -227,8 +185,7 @@ cudaError_t ioRtConsistencyInit()
 
 cudaError_t ioRtConsistencyFenceCurrentCtx()
 {
-    ASSERT(iocons);
-    return (cudaError_t)iocons->ioConsistencyFenceCurrentCtx();
+    return (cudaError_t)ioConsistencyFenceCurrentCtx();
 }
 
 cudaError_t ioRtConsistencyDeviceSupportsCpuFlush(int *pflag, int device)
